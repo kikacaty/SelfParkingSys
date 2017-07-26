e8ee9bcfe984604654d1b450f63950b18c3b4e35
Yulongc <yulongc@umich.edu>
2017-07-26 10:39:32 +0800

    update

--- serial_control/serial_console.cpp
+++ serial_control/serial_console.cpp
@@ -1,65 +1,63 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <stdio.h>
+#include <chrono>
+#include <thread>
+
+
+const int INIT_DIR=240;
+const int INIT_SPEED=250;
+
 
 int main(int argc, char* argv[])
 {
 	//direction range: 100 ~ 600, center: 340
-	//speed range: 240 ~ 320, center: 280
+	//speed range: 200 ~ 320, center: 280
 	//open arduino device file (linux)
-	std::ofstream arduino;
+	FILE *arduino;
 
 	char control;
 	bool isEnd = false;
 
-	int direction = 340;
-	int speed = 280;
+	int direction = INIT_DIR;
+	int speed = INIT_SPEED;
 	bool isDash = false;
+
+	arduino = fopen( "/dev/ttyACM0", "w");
 
 	while (!isEnd) {
 		std::cout << "Speed: " << speed << '\t' << "Direction: " << direction << std::endl;
-		
-		std::cin >> control;
-		
 
-		arduino.open( "/dev/ttyACM0");
+		std::cin >> control;
+
 		if (control == 'a') direction -= 20;
 		else if (control == 'd') direction += 20;
 		else if (control == 'w') speed += 5;
 		else if (control == 's') speed -= 5;
 		else if (control == 'r') {
-			arduino << "s " << 280;
-			arduino.close();
-			arduino.open("/dev/ttyACM0");
-			arduino << "d " << 340;
-			speed = 280; 
-			direction = 340;}
+			fprintf(arduino, "s %d\n", INIT_SPEED);
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			fprintf(arduino, "d %d\n", INIT_DIR);
+			speed = INIT_SPEED;
+			direction = INIT_DIR;
+			}
 		else if (control == 'e') isEnd = true;
-		else if (control == 'p') {
-			arduino << "s " << 320;
-			arduino.close();
-			arduino.open("/dev/ttyACM0");
-			usleep(50);
-			arduino << "s " << 280;
-			speed = 280;
-		}
-		else if (control == 'l') {
-			arduino << "s " << 235;
-			arduino.close();
-			arduino.open("/dev/ttyACM0");
-			usleep(50);
-			arduino << "s " << 280;
-			speed = 280;
+		else if (control == 'b') {
+			fprintf(arduino, "s 200\n");
+			std::this_thread::sleep_for(std::chrono::milliseconds(200));
+			fprintf(arduino, "s 205\n");
+			speed = 205;
 		}
 		//write to it
 
-		if (control == 'd' || control == 'a') arduino << "d " << direction;
-		else if (control == 'w' || control == 's') arduino << "s " << speed;
-		
+		if (control == 'd' || control == 'a') fprintf(arduino, "d %d\n", direction);
+		else if (control == 'w' || control == 's') fprintf(arduino, "s %d\n",speed);
+	
 
-		arduino.close();
 	}
 
+	fclose(arduino);
 
 	return 0;
 
